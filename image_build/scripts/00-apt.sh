0bf211ae70b0d19426af43c22deadd2d88cc4287
eatradish <sakiiily@aosc.io>
2023-11-10 02:15:47 +0000

    chore: remove useless file

--- image_build/scripts/00-apt.sh
+++
@@ -1,6 +0,0 @@
-apt-get remove -y linux-kernel*
-apt-get update
-apt-get install -y grub
-
-wget -O kernel.deb "https://repo.aosc.io/misc/linux-kernel-asahi-5.18.0_5.18.0-0_arm64.deb"
-dpkg -i ./kernel.deb
